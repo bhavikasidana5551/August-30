33081c1b7b4ded0067591888b6c22a4934d16781
bhavikasidana5551 <bhavikasidana5551@gmail.com>
2025-08-30 22:47:44 +0530

    Add files via upload

---
+++ august30.g.c
@@ -0,0 +1,15 @@
+//use 1printf to print, 2int x is first declared then used with %d, 3\n to use enter
+#include <stdio.h>
+int main()
+{
+    int x;
+    x=1;
+    printf("%d",x );
+    printf(". my name is Bhavika Sidana\n");
+    int y;
+    y = 2;
+    printf("%d",y);
+    printf(". it's my first time on earth so please forbid me for any mistake!");
+
+    return 0;
+}
